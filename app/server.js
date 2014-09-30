2af3d96dab8e401c2767ffc7f59cac5ce21d2513
Matías Hernández Arellano <matiasfh@gmail.com>
2014-09-30 16:24:08 -0300

    new model for news and added ebooks models and search

--- app/server.js
+++
@@ -1,9 +0,0 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var morgan = require('morgan');
-var app = express();
-
-app.use(morgan('dev'));
-//app.use(gzippo.staticGzip("."));
-app.use(express.static('.'));
-app.listen(process.env.PORT || 5000);
