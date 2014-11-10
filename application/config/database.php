4b8a7fed1bc73cd49603df68db3f48f0e836c759
Matias Hernandez <matiasfh@gmail.com>
2014-11-10 15:57:01 -0300

    new config

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'developer';
-$db['default']['password'] = 'developer';
-$db['default']['database'] = 'development';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '123';
+$db['default']['database'] = 'sibib';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'sibib_';
 $db['default']['pconnect'] = TRUE;
