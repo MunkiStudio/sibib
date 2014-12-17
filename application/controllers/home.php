c3a6aa7d16c922af49abd0853868d18edacaadbf
Matias Hernandez <matiasfh@gmail.com>
2014-12-17 15:31:00 -0300

    new build

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -10,8 +10,9 @@ class Home extends CI_Controller {
 	private function _init(){
 		$this->output->set_template('default');
 		date_default_timezone_set('America/Santiago');
+    $this->load->model('News', 'news');
 	}
-	
+
 	public function index()
 	{
 
